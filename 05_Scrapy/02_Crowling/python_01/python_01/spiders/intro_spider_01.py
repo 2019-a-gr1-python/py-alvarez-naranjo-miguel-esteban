86056229885ab9ce525a8d429dab6d54382b1778
MiguelAlvarez97 <miguel9730@hotmail.com>
2019-06-29 09:29:25 -0500

    follow link

--- 05_Scrapy/02_Crowling/python_01/python_01/spiders/intro_spider_01.py
+++ 05_Scrapy/02_Crowling/python_01/python_01/spiders/intro_spider_01.py
@@ -1,4 +1,5 @@
 import scrapy
+import pandas as pd
 class IntroSpider(scrapy.Spider):
     name = 'introduccion_spider'
 
@@ -27,3 +28,7 @@ class IntroSpider(scrapy.Spider):
         print(stocks)
         print(precios)
         print(diccionario)
+
+        df = pd.DataFrame(diccionario)
+
+        df.to_excel('librosScrapy.xlsx')
