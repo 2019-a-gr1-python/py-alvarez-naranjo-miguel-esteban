a3992aa3399fe8a3a32b8e77983739072d2e353e
MiguelAlvarez97 <miguel9730@hotmail.com>
2019-07-31 03:24:04 -0500

    json de delitos ecuador

--- Proyecto 2do Bim/scrapyDelitosEC/delitosEC/delitosEC/spiders/spider_delitosEC.py
+++ Proyecto 2do Bim/scrapyDelitosEC/delitosEC/delitosEC/spiders/spider_delitosEC.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 
 class AraniaFiscaliaGeneralEstadoEC(scrapy.Spider):
     name = 'arania_delitosEC'
@@ -6,6 +7,8 @@ class AraniaFiscaliaGeneralEstadoEC(scrapy.Spider):
     def start_requests(self):
         urls = [
             'https://www.gestiondefiscalias.gob.ec/siaf/comunes/noticiasdelito/info_mod.php?businfo=a:2:{i:0;s:6:%22miguel%22;i:1;s:7:%22esteban%22;}',
+            'https://www.gestiondefiscalias.gob.ec/siaf/comunes/noticiasdelito/info_mod.php?businfo=a:2:{i:0;s:5:%22paola%22;i:1;s:5:%22maria%22;}',
+            'https://www.gestiondefiscalias.gob.ec/siaf/comunes/noticiasdelito/info_mod.php?businfo=a:2:{i:0;s:4:%22juan%22;i:1;s:4:%22jose%22;}',
         ]
 
         for url in urls:
@@ -15,8 +18,9 @@ class AraniaFiscaliaGeneralEstadoEC(scrapy.Spider):
         
         for tabla in response.xpath('//*[@class="general"]//tbody'):
             existe_tabla = len(tabla.xpath('tr//td//text()').extract())
+            lugar = re.search('LUGAR', tabla.xpath('tr//td//text()')[0].extract())
             print(existe_tabla)
-            if(existe_tabla > 13):
+            if(existe_tabla > 13 and lugar):
                 yield {
                     tabla.xpath('tr//td//text()')[0].extract():tabla.xpath('tr//td//text()')[1].extract(),
                     tabla.xpath('tr//td//text()')[2].extract():tabla.xpath('tr//td//text()')[3].extract(),
