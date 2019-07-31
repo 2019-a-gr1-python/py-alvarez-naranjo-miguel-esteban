1a7be49f80d621954a2987c1894ed86593099a55
MiguelAlvarez97 <miguel9730@hotmail.com>
2019-07-31 02:05:53 -0500

    arania delitos

--- Proyecto 2do Bim/scrapyDelitosEC/delitosEC/delitosEC/spiders/spider_delitosEC.py
+++ Proyecto 2do Bim/scrapyDelitosEC/delitosEC/delitosEC/spiders/spider_delitosEC.py
@@ -8,4 +8,16 @@ class AraniaFiscaliaGeneralEstadoEC(scrapy.Spider):
             'https://www.gestiondefiscalias.gob.ec/siaf/comunes/noticiasdelito/info_mod.php?businfo=a:2:{i:0;s:6:%22miguel%22;i:1;s:7:%22esteban%22;}',
         ]
 
-        
+        for url in urls:
+            yield scrapy.Request(url = url, callback=self.parse)
+    
+    def parse(self, response):
+        tablas = response.xpath('//*[@class="general"]//tbody')
+        for tabla in tablas:
+            yield {
+                tablas.xpath('tr//td//text()')[0]:tablas.xpath('tr//td//text()')[1],
+                tablas.xpath('tr//td//text()')[2]:tablas.xpath('tr//td//text()')[3],
+                tablas.xpath('tr//td//text()')[4]:tablas.xpath('tr//td//text()')[5],
+                tablas.xpath('tr//td//text()')[11]:tablas.xpath('tr//td//text()')[12],
+
+            }
